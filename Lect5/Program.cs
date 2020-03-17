8b2904efad15456f8adce5a9a94fae116757f5ce
Alexandr <Alexandr@ALEXANDR->
2020-03-17 23:23:49 +0300

    Task2 Done

---
+++ Lect5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lect5.LetterA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a')
+                {
+                    sum++;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
